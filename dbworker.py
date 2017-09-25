d9a8b41c530b77edac9ca430d4eb7f6ef055a927
ye11owSub <casio00@mail.ru>
2017-09-26 00:05:32 +0300

    dbworker update

--- dbworker.py
+++ dbworker.py
@@ -1,4 +1,5 @@
 import pg
+from helpers import load_config, create_query_text
 from helpers import create_query_text
 
 error_message = "Параметры указаны не верно"
@@ -31,20 +32,21 @@ class Donor:
 
     @staticmethod
     def try_exist(user_id: int):
-        query_text = 'SELECT count(*) FROM "Donor" WHERE "id"=$1'
+        query_text = 'SELECT EXISTS (SELECT 1 FROM "Donor" WHERE "id"=$1)'
         with pg.DB(**CONFIG_PARAMS) as conn:
-            count = conn.query(query_text, user_id).getresult()
-            return count[0][0] == 1
+            return conn.query(query_text, user_id).getresult()[0][0]
 
     @staticmethod
     def get_donor_data(user_id: int) -> tuple:
-        query_text = 'SELECT * FROM "Donor" WHERE "id" = $1'
+        query_text = 'SELECT * FROM "Donor" WHERE "id"=$1'
         with pg.DB(**CONFIG_PARAMS) as conn:
             return conn.query(query_text, user_id).getresult()[0]
 
     @staticmethod
     def update_with_data(donor_data: dict):
         if donor_data:
+            query_text = 'UPDATE "Donor" SET ({columns}) = ({values}) WHERE "id" = ${user_param}'
+            columns, values_len, values = create_query_text(donor_data)
             query_text = 'UPDATE "Donor" SET ({columns}) = ({values}) WHERE "id" = {user_param}'
             columns, values_len, values = create_query_text(donor_data)
             query = query_text.format(columns=columns, values=values_len, user_param=donor_data["donor_id"])
@@ -74,6 +76,8 @@ class Request:
 
     @staticmethod
     def update_request(request_info, request_id):
+        query_text = 'UPDATE "Request" SET ({columns}) = ({values}) WHERE "request_id" = ${request_param}'
+        columns, values_len, values = create_query_text(request_info)
         query_text = 'UPDATE "Request" SET ({columns}) = ({values}) WHERE "request_id" = {request_param}'
         columns, values_len, values = create_query_text(request_info)
         query = query_text.format(columns=columns, values=values_len, request_param=request_id)
@@ -88,9 +92,11 @@ class Request:
             return empty_exist[0][0]  # возвращаю номер запроса
 
         else:
+            query_text = 'INSERT INTO "Request" ({columns}) VALUES ({values}) RETURNING request_id'
+            columns, values_len, values = create_query_text(request_info)
             query_text = 'INSERT INTO "Request" ({columns}) VALUES ({values})'
             columns, values_len, values = create_query_text(request_info)
             query = query_text.format(columns=columns, values=values_len)
             with pg.DB(**CONFIG_PARAMS) as conn:
-                conn.query(query, *values)
+                return conn.query(query, *values).getresult()[0][0]
                 # return request_id
